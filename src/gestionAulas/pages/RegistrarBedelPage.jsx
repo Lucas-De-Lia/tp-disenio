1e8f06422e53ccc3b82a00075acde09cedaaf037
Lucas De Lia <lucasdelia018@gmail.com>
2024-10-31 16:39:00 -0300

    Modal succes listo, falta modal error y separar el componente en componentes mas chicos

--- src/gestionAulas/pages/RegistrarBedelPage.jsx
+++ src/gestionAulas/pages/RegistrarBedelPage.jsx
@@ -11,6 +11,7 @@ import {
 } from "@mui/material";
 import { Header } from "../../components";
 import {
+  CheckCircleOutlineOutlined,
   LockOutlined,
   VisibilityOffOutlined,
   VisibilityOutlined,
@@ -27,23 +28,35 @@ export const RegistrarBedelPage = () => {
     nombre,
     apellido,
     turno,
-    contrasenia,
-    confirmarContrasenia,
-    userId,
+    password,
+    repeatedPassword,
+    idUsuario,
     onInputChange,
+    onResetForm
   } = useForm({
     nombre: "",
     apellido: "",
     turno: "",
-    contrasenia: "",
-    confirmarContrasenia: "",
-    userId: "",
+    password: "",
+    repeatedPassword: "",
+    idUsuario: "",
   });
 
   const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirmation, setShowPasswordConfirmation] =
-    useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
   const [modal, setModal] = useState(false);
+  
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleClose = () => {
+    setSuccess(false);
+    onResetForm();
+}
+const handleSuccessExit = () => {
+    setSuccess(false);
+    navigate('/dashboard');
+}
 
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
@@ -66,14 +79,14 @@ export const RegistrarBedelPage = () => {
 
     // TODO modificar el form o el back para no tener que hacer esto
     const data = {
-      idUsuario: userId,
-      apellido,
-      habilitado: true,
+      idUsuario,
       nombre,
-      password: contrasenia,
-      repeatedPassword: confirmarContrasenia,
-      registradoPor: "admin",
+      //habilitado: true, no es necesario
+      apellido,
+      password,
+      repeatedPassword,
       turno,
+      registradoPor: JSON.parse(localStorage.getItem("user")).user
     };
 
     try {
@@ -83,14 +96,15 @@ export const RegistrarBedelPage = () => {
       };
 
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/create/bedel`,
+        "http://localhost:8080/api/create/bedel",
         data,
         { headers }
       );
       console.log("Response:", response.data);
-
+      setSuccess(true);
+      setError(null);
       // TODO Manejar caso success, creo q era mostrar un modal de exito que dsp te lleve al dashboard
-      //navigate("/dashboard");
+      //navigate("/dashboard"); lo pase al modal de exito
     } catch (error) {
       console.error("Error:", error);
       // TODO Manejar caso error, que se settee el mensaje del back en el form
@@ -166,8 +180,8 @@ export const RegistrarBedelPage = () => {
                     Contraseña
                   </Typography>
                   <TextField
-                    name="contrasenia"
-                    value={contrasenia}
+                    name="password"
+                    value={password}
                     onChange={onInputChange}
                     type={showPassword ? "text" : "password"}
                     slotProps={{
@@ -216,17 +230,17 @@ export const RegistrarBedelPage = () => {
                     Identificador de usuario
                   </Typography>
                   <TextField
-                    name="userId"
+                    name="idUsuario"
                     type="number"
-                    value={userId}
+                    value={idUsuario}
                     onChange={onInputChange}
                   />
                   <Typography color="#5E6366" ml={1} mt={5}>
                     Confirmar contraseña
                   </Typography>
                   <TextField
-                    name="confirmarContrasenia"
-                    value={confirmarContrasenia}
+                    name="repeatedPassword"
+                    value={repeatedPassword}
                     onChange={onInputChange}
                     type={showPasswordConfirmation ? "text" : "password"}
                     slotProps={{
@@ -343,6 +357,72 @@ export const RegistrarBedelPage = () => {
               >
                 Registrar bedel
               </Button>
+              <Modal open = {success} onClose={handleClose }>
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    background: "white",
+                    width: "35%",
+                    height: "35%",
+                    borderRadius: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography variant="h5" component="h5" mt={5}>
+                    <Box
+                      display='flex'
+                      alignItems='center'
+                      justifyContent='center'
+                      gap={2}
+                    >
+                      El bedel se ha registrado con exito! 
+                      <CheckCircleOutlineOutlined
+                          sx={{ color: "#32936F", fontSize: 60 }}
+                      />
+                    </Box>
+                  </Typography>
+                  <Box
+                    display="flex"
+                    justifyContent="space-around"
+                    alignItems="center"
+                    mt={5}
+                  >
+                    <Box display="flex" alignItems="center" gap={5}>
+                      <Button
+                        variant="outlined"
+                        size="medium"
+                        sx={{
+                          width: "200px",
+                          color: "#32936F",
+                          borderColor: "#32936F",
+                          height: "50px"
+                        }}
+                        onClick={handleClose}
+                      >
+                        Registrar otro bedel
+                      </Button>
+                      <Button
+                        variant="contained"
+                        size="medium"
+                        sx={{
+                          width: "200px",
+                          backgroundColor: "#32936F",
+                          height: "50px"
+                        }}
+                        onClick={handleSuccessExit}
+                      >
+                        Salir
+                      </Button>
+                    </Box>
+                  </Box>
+                </Box>
+                </Modal>
             </Box>
           </Box>
         </Box>
