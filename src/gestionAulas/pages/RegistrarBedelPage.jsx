cd82e591c8ce4f8de25390e8193e3a60554631f2
Gonzalo Aguero <gonzaloaguerodev@gmail.com>
2024-10-31 21:55:10 -0300

    Se agregó modal de error en registrar bedel Se agregó texto con los errores específicos en el formulario.

--- src/gestionAulas/pages/RegistrarBedelPage.jsx
+++ src/gestionAulas/pages/RegistrarBedelPage.jsx
@@ -8,10 +8,13 @@ import {
   Select,
   TextField,
   Typography,
+  List,
+  ListItem,
 } from "@mui/material";
 import { Header } from "../../components";
 import {
   CheckCircleOutlineOutlined,
+  ErrorOutline,
   LockOutlined,
   VisibilityOffOutlined,
   VisibilityOutlined,
@@ -32,7 +35,7 @@ export const RegistrarBedelPage = () => {
     repeatedPassword,
     idUsuario,
     onInputChange,
-    onResetForm
+    onResetForm,
   } = useForm({
     nombre: "",
     apellido: "",
@@ -43,20 +46,27 @@ export const RegistrarBedelPage = () => {
   });
 
   const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirmation, setShowPasswordConfirmation] = useState(false);
+  const [showPasswordConfirmation, setShowPasswordConfirmation] =
+    useState(false);
   const [modal, setModal] = useState(false);
-  
+
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const [errorList, setErrorList] = useState(null);
 
   const handleClose = () => {
     setSuccess(false);
     onResetForm();
-}
-const handleSuccessExit = () => {
+  };
+
+  const handleErrorModal = (state) => {
+    setError(state);
+  };
+
+  const handleSuccessExit = () => {
     setSuccess(false);
-    navigate('/dashboard');
-}
+    navigate("/dashboard");
+  };
 
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
@@ -86,7 +96,7 @@ const handleSuccessExit = () => {
       password,
       repeatedPassword,
       turno,
-      registradoPor: JSON.parse(localStorage.getItem("user")).user
+      registradoPor: JSON.parse(localStorage.getItem("user")).user,
     };
 
     try {
@@ -104,10 +114,10 @@ const handleSuccessExit = () => {
       setSuccess(true);
       setError(null);
       // TODO Manejar caso success, creo q era mostrar un modal de exito que dsp te lleve al dashboard
-      //navigate("/dashboard"); lo pase al modal de exito
-    } catch (error) {
-      console.error("Error:", error);
-      // TODO Manejar caso error, que se settee el mensaje del back en el form
+    } catch (err) {
+      console.error("Error:", err);
+      setError(true);
+      setErrorList(err.response.data);
     }
   };
 
@@ -119,19 +129,20 @@ const handleSuccessExit = () => {
           sx={{
             background:
               "linear-gradient(180deg, #32936F 30% , #328880 60%,#32838A 74%,#29768D 88%)",
-            height: "100vh",
+            minHeight: "100vh",
             width: "100%",
-            mt: 1,
+            paddingY: 5,
             display: "flex",
             justifyContent: "center",
-            alignItems: "center",
           }}
         >
+          {/* Contenedor formulario */}
           <Box
             sx={{
               background: "white",
               width: "60%",
-              height: "90%",
+              minHeight: "90%",
+              paddingY: 7,
               borderRadius: 2,
             }}
           >
@@ -206,8 +217,9 @@ const handleSuccessExit = () => {
                     }}
                   />
                   <Typography color="#5E6366" ml={1} variant="caption">
-                    La contraseña debe contener al menos 8 caracteres, incluir
-                    una mayúscula e incluir un número.
+                    La contraseña debe contener al menos 8 caracteres, y al
+                    menos incluir una mayúscula, un número y un signo especial
+                    (@#$%&*).
                   </Typography>
                 </Box>
                 <Box
@@ -267,12 +279,58 @@ const handleSuccessExit = () => {
                     }}
                   />
                   <Typography color="#5E6366" ml={1} variant="caption">
-                    La contraseña debe contener al menos 8 caracteres, incluir
-                    una mayúscula e incluir un número.
+                    La contraseña debe contener al menos 8 caracteres, y al
+                    menos incluir una mayúscula, un número y un signo especial
+                    (@#$%&*).
                   </Typography>
                 </Box>
               </Box>
             </Box>
+
+            {errorList !== null && (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row nowrap",
+                  gap: 2,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginTop: 5,
+                }}
+              >
+                <ErrorOutline sx={{ color: "#2B2F32", fontSize: 40 }} />
+                <List
+                  sx={{
+                    listStyleType: "none",
+                    padding: 1,
+                    bgcolor: "#EDEDED",
+                    borderRadius: 3,
+                    width: "45%",
+                  }}
+                >
+                  {Object.entries(errorList).map(([field, message], index) => (
+                    <ListItem key={index + field}>
+                      <Typography
+                        sx={{
+                          color: "#2B2F32",
+                          fontSize: 14,
+                          width: "100%",
+                        }}
+                      >
+                        {`${message}`}
+                      </Typography>
+                    </ListItem>
+                  ))}
+                </List>
+              </Box>
+            )}
+
+            {/* 
+                  
+                  BOTONES DE CANCELAR Y REGISTRAR
+
+            */}
+
             <Box
               display="flex"
               justifyContent="center"
@@ -292,6 +350,11 @@ const handleSuccessExit = () => {
               >
                 Cancelar
               </Button>
+              {/* 
+                
+                    MODAL DE CANCELAR
+                
+              */}
               <Modal open={modal} onClose={() => handleModal(false)}>
                 <Box
                   sx={{
@@ -357,7 +420,12 @@ const handleSuccessExit = () => {
               >
                 Registrar bedel
               </Button>
-              <Modal open = {success} onClose={handleClose }>
+              {/* 
+                
+                  MODAL DE EXITO
+                
+                */}
+              <Modal open={success} onClose={handleClose}>
                 <Box
                   sx={{
                     position: "absolute",
@@ -376,14 +444,14 @@ const handleSuccessExit = () => {
                 >
                   <Typography variant="h5" component="h5" mt={5}>
                     <Box
-                      display='flex'
-                      alignItems='center'
-                      justifyContent='center'
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
                       gap={2}
                     >
-                      El bedel se ha registrado con exito! 
+                      El bedel se ha registrado con exito!
                       <CheckCircleOutlineOutlined
-                          sx={{ color: "#32936F", fontSize: 60 }}
+                        sx={{ color: "#32936F", fontSize: 60 }}
                       />
                     </Box>
                   </Typography>
@@ -401,7 +469,7 @@ const handleSuccessExit = () => {
                           width: "200px",
                           color: "#32936F",
                           borderColor: "#32936F",
-                          height: "50px"
+                          height: "50px",
                         }}
                         onClick={handleClose}
                       >
@@ -413,7 +481,7 @@ const handleSuccessExit = () => {
                         sx={{
                           width: "200px",
                           backgroundColor: "#32936F",
-                          height: "50px"
+                          height: "50px",
                         }}
                         onClick={handleSuccessExit}
                       >
@@ -422,7 +490,66 @@ const handleSuccessExit = () => {
                     </Box>
                   </Box>
                 </Box>
-                </Modal>
+              </Modal>
+              {/* 
+                
+                    MODAL DE ERROR
+                
+                */}
+              <Modal open={error} onClose={() => handleErrorModal(false)}>
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    background: "white",
+                    width: "35%",
+                    height: "35%",
+                    borderRadius: 2,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: 5,
+                  }}
+                >
+                  <Typography variant="h6" component="h5" mt={5}>
+                    <Box
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="center"
+                      textAlign="center"
+                      gap={2}
+                    >
+                      Algunos campos no son válidos, por favor verifique los
+                      datos ingresados.
+                      <ErrorOutline sx={{ color: "#32936F", fontSize: 60 }} />
+                    </Box>
+                  </Typography>
+                  <Box
+                    display="flex"
+                    justifyContent="space-around"
+                    alignItems="center"
+                    mt={5}
+                  >
+                    <Box display="flex" alignItems="center" gap={5}>
+                      <Button
+                        variant="contained"
+                        size="medium"
+                        sx={{
+                          width: "200px",
+                          backgroundColor: "#32936F",
+                          height: "50px",
+                        }}
+                        onClick={() => handleErrorModal(false)}
+                      >
+                        Aceptar
+                      </Button>
+                    </Box>
+                  </Box>
+                </Box>
+              </Modal>
             </Box>
           </Box>
         </Box>
