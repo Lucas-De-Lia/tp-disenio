e85f6c8c62cfc2cd8f2a3c48b0b3814e990ca112
Lucas De Lia <lucasdelia018@gmail.com>
2024-11-19 19:06:30 -0300

    comiendo BuscarBedelPage

--- src/gestionAulas/pages/BuscarBedelPage.jsx
+++ src/gestionAulas/pages/BuscarBedelPage.jsx
@@ -1,47 +1,104 @@
-import React, { useState, useEffect } from 'react';
+import { Box, InputAdornment, MenuItem, Select, TextField, Typography } from "@mui/material"
+import { Header } from "../../components"
+import { Search } from "@mui/icons-material"
 
-/**
- * Nota: La lógica no está implementada, solo son funciones que probablemente usemos después.
- */
 export const BuscarBedelPage = () => {
-    const [bedeles, setBedeles] = useState([]);
-    const [nombre, setNombre] = useState('');
+  return (
+    <>
+        <Header/>
+        <Box
+            sx={{
+                background:"linear-gradient(180deg, #32936F 30% , #328880 60%,#32838A 74%,#29768D 88%)",
+                minHeight: "100vh",
+                minwidth: "100vw",
+                display:"flex",
+                alignItems:"center",
+                justifyContent:"center"
+            }}
+        >
+            <Box
+                sx={{
+                    background:"white",
+                    marginTop:"50px",
+                    borderRadius:2,
+                    width: "60%",
+                    minHeight: "90vh",
+                }}
+            >
+                <Typography
+                    variant="h4"
+                    component="h4"
+                    textAlign="center"
+                    mt={6}
+                    mb={3}
+                >
+                    Buscar bedel
+                </Typography>
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    justifyContent="space-evenly"
+                    alignItems="center"
 
-    useEffect(() => {
-        // Fetch bedeles based on the entered name
-        if (nombre) {
-            fetchBedelesPorNombre(nombre);
-        }
-    }, [nombre]);
-
-    const fetchBedelesPorNombre = async (nombre) => {
-        try {
-            const response = await fetch(`/api/bedeles?nombre=${nombre}`);
-            const data = await response.json();
-            setBedeles(data);
-        } catch (error) {
-            console.error('Error fetching bedeles:', error);
-        }
-    };
-
-    const handleNombreChange = (event) => {
-        setNombre(event.target.value);
-    };
-
-    return (
-        <div>
-            <h1>Buscar Bedel</h1>
-            <input 
-                type="text" 
-                value={nombre} 
-                onChange={handleNombreChange} 
-                placeholder="Ingrese el nombre del bedel"
-            />
-            <ul>
-                {bedeles.map((bedel) => (
-                    <li key={bedel.id}>{bedel.nombre}</li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+                >
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        gap="2px"
+                        width="40%"
+                    >
+                        <Typography
+                            htmlFor ="buscador"
+                            color="#5E6366"
+                            variant="caption"
+                            component="label"
+                            ml={1}
+                        >
+                            Términos de busqueda
+                        </Typography>
+                        <TextField
+                            id = 'buscador'
+                            type="search"
+                            slotProps={{
+                                input:{
+                                    startAdornment:(
+                                        <InputAdornment position="start">
+                                            <Search/>
+                                        </InputAdornment>
+                                    )
+                                }
+                            }}
+                        />
+                    </Box>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                        gap="2px"
+                        width="40%"
+                    >
+                        <Typography
+                            htmlFor ="filtro"
+                            color="#5E6366"
+                            variant="caption"
+                            component="label"
+                            ml={1}
+                        >
+                            Filtro
+                        </Typography>
+                        <Select
+                            labelId="filtro"
+                            id="filtro"
+                            MenuProps={{
+                                disableScrollLock: true, // Evita bloquear el desplazamiento del body
+                            }}
+                        >
+                            <MenuItem value = "Apellido">Apellido</MenuItem>
+                            <MenuItem value = "Turno" >Turno</MenuItem>
+                        </Select>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    </>
+  )
+}
